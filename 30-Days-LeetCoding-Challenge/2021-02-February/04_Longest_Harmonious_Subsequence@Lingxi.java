392305385cbbb2c5244528f4cb744ed697c6d398
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2021-02-04 19:36:39 -0500

    Update 04_Longest_Harmonious_Subsequence@Lingxi.java

--- 30-Days-LeetCoding-Challenge/2021-02-February/04_Longest_Harmonious_Subsequence@Lingxi.java
+++ 30-Days-LeetCoding-Challenge/2021-02-February/04_Longest_Harmonious_Subsequence@Lingxi.java
@@ -20,3 +20,23 @@ class Solution {
         return lastCount != -1 ? Math.max(output, lastCount + count) : output;
     }
 }
+
+/**
+    Another approach:
+        class Solution {
+            public int findLHS(int[] nums) {
+                Map<Integer, Integer> map = new HashMap();
+                for (int i : nums) {
+                    map.put(i, map.getOrDefault(i, 0) + 1);
+                }
+                int output = 0;
+                for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                    int key = entry.getKey();
+                    if (map.containsKey(key + 1)) {
+                        output = Math.max(output, map.get(key) + map.get(key + 1));
+                    }
+                }
+                return output;
+            }
+        }
+*/
