ba815bd9464414e374dae768876d6d53c0fbd83c
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2021-02-07 09:13:38 -0500

    Create 03_Linked_List_Cycle@Lingxi.java

---
+++ 30-Days-LeetCoding-Challenge/2021-02-February/03_Linked_List_Cycle@Lingxi.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet();
+        while (head != null) {
+            if (!set.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
