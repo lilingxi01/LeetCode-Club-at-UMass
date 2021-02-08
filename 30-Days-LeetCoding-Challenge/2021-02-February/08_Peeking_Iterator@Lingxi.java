6402f3d0b3be72e3d6ca7ddb444e9666a1a85b9b
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2021-02-08 16:29:01 -0500

    Create 08_Peeking_Iterator@Lingxi.java

---
+++ 30-Days-LeetCoding-Challenge/2021-02-February/08_Peeking_Iterator@Lingxi.java
@@ -0,0 +1,41 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+class PeekingIterator implements Iterator<Integer> {
+    private Integer next;
+    private Iterator<Integer> itr;
+    private boolean hasNext;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+        this.itr = iterator;
+        this.hasNext = itr.hasNext();
+        if (hasNext) {
+	        this.next = itr.next();
+        }
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return next;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    Integer output = next;
+        if (itr.hasNext()) {
+            next = itr.next();
+        } else {
+            next = null;
+            hasNext = false;
+        }
+        return output;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return hasNext;
+	}
+}
