62e6e7d1072509e3238463cc67b1125bfbc4a8b5
Lingxi_Li <36816148+lilingxi01@users.noreply.github.com>
2021-02-17 20:38:24 -0500

    Create 17_Container_With_Most_Water@Lingxi.java

---
+++ 30-Days-LeetCoding-Challenge/2021-02-February/17_Container_With_Most_Water@Lingxi.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, max = 0;
+        while (left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
